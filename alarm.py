fdf94b77ad961adc3a091e02642ef3147d43f4c7
will-wrigh <43973227+will-wrigh@users.noreply.github.com>
2020-03-03 15:37:01 -0500

    Contains file that can use alarm sound byte
    Python file can utilize .wav file to make sound however file setup needs to be customized when downloaded to user's individual system

---
+++ alarm.py
@@ -0,0 +1,24 @@
+from os import listdir
+import pygame
+import schedule
+import time
+
+def job():
+	location = 'home/pi/Documents/alarm/alarm.wav'
+	#ensure file name is the same
+	pygame.init()
+	pygame.mixer.init()
+	pygame.mixer.music.load(location)
+	pygame.mixer.music.play(0)
+	#loads sound
+
+
+schedule.every().day.at("15:00").do(job)
+#schedule.every(1).minutes.do(job)
+#for quick schedule testing
+
+	while True:
+		schedule.run_pending()
+		time.sleep(1)
+		#job()
+		#for quick sound testing
